82e641402f400ed09aab79ca81de55a7abe0a0db
jean.dominguez <jean.dominguez@btilab.co>
2024-06-05 13:25:04 -0500

    feat: chanes styles in component singin

--- src/Pages/auth/SingIn.tsx
+++ src/Pages/auth/SingIn.tsx
@@ -4,70 +4,73 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Button, Input, Paragraph, Title } from "../../Components";
 import appFirebase from "../../Credentials";
 
 const auth = getAuth(appFirebase);
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 30px;
+  padding-right: 30px;
+  margin: auto;
+  width: auto;
+  height: 100%;
+  background-color: #181828;
+`;
+
+const ContainerInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+  margin-bottom: 22px;
+`;
+
+const ContainerLogin = styled.div`
+  margin-top: 22px;
+`;
+const ContainerParagrahp = styled.div`
+  margin-top: 7px;
+  margin-bottom: 23px;
+  text-align: center;
+`;
+
+const ContainerParagrahpLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const ButtonRegister = styled.button`
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 12px;
+  border: none;
+  height: 32px;
+  text-align: center;
+  background: linear-gradient(90deg, #236bfe 0%, #0d4ed3 100%);
+`;
+const ContainerRegister = styled.div`
+  margin-top: 20px;
+  gap: 12px;
+  display: flex;
+  align-items: center;
+`;
+
+const ParagraphRegister = styled.p`
+  margin: 0;
+  color: ${({ theme }) => theme.paragraph};
+`;
 
 export const SingIn = () => {
+  const navigate = useNavigate();
   const [register, setRegister] = useState(false);
-  const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-left: 30px;
-    padding-right: 30px;
-    margin: auto;
-    width: auto;
-    height: 100%;
-    background-color: #181828;
-  `;
-
-  const ContainerInput = styled.div`
-    display: flex;
-    flex-direction: column;
-    row-gap: 12px;
-    margin-bottom: 22px;
-  `;
-
-  const ContainerLogin = styled.div`
-    margin-top: 22px;
-  `;
-  const ContainerParagrahp = styled.div`
-    margin-top: 7px;
-    margin-bottom: 23px;
-    text-align: center;
-  `;
-
-  const ContainerParagrahpLogin = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  `;
-
-  const ButtonRegister = styled.button`
-    display: flex;
-    padding: 20px;
-    align-items: center;
-    justify-content: center;
-    border-radius: 12px;
-    border: none;
-    height: 32px;
-    text-align: center;
-    background: linear-gradient(90deg, #236bfe 0%, #0d4ed3 100%);
-  `;
-  const ContainerRegister = styled.div`
-    margin-top: 20px;
-    gap: 12px;
-    display: flex;
-    align-items: center;
-  `;
-
-  const ParagraphRegister = styled.p`
-    margin: 0;
-  `;
 
   const onSubmitLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -79,6 +82,7 @@ export const SingIn = () => {
       try {
         await createUserWithEmailAndPassword(auth, email, password);
         alert("created in successfully!");
+        navigate("/Home");
       } catch (error) {
         alert("make sure your password is more than 8 characters long");
       }
@@ -86,6 +90,7 @@ export const SingIn = () => {
       try {
         await signInWithEmailAndPassword(auth, email, password);
         alert("Logged in successfully!");
+        navigate("/Home");
       } catch (error) {
         alert("Failed to log in. Please check your credentials.");
       }

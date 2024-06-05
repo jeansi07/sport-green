bfd26f94e4d2b7f53447630cdaa84ffc7d6eac70
jean.dominguez <jean.dominguez@btilab.co>
2024-06-05 13:26:15 -0500

    feat: new pages view logout

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { History, Home } from "../Pages";
+import { History, Home, SingIn } from "../Pages";
+import { Logout } from "../Pages/Logout";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/history" element={<History />} />
+      <Route path="/*" element={<SingIn />} />
+      <Route path="/logout" element={<Logout />} />
     </Routes>
   );
 };
